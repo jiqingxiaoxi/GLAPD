a89642f94d0069039722ca7b4d04a3674a9d4122
Jia Ben <chenmodexiaoxi@126.com>
2019-05-05 20:52:07 +0800

    Add files via upload

--- single.c
+++ single.c
@@ -6,6 +6,7 @@
 #include<unistd.h>
 #include<time.h>
 #include<sys/stat.h>
+#include<ctype.h>
 
 char str2int(char c)
 {
@@ -2813,7 +2814,7 @@ void usage()
 	printf("    print usage\n");
 }
 
-main(int argc, char **argv) 
+void main(int argc, char **argv) 
 {
 	double stackEntropies[625],stackEnthalpies[625],stackint2Entropies[625],stackint2Enthalpies[625],dangleEntropies3[125],dangleEnthalpies3[125],dangleEntropies5[125],dangleEnthalpies5[125];
         double hairpinLoopEntropies[30],interiorLoopEntropies[30],bulgeLoopEntropies[30],hairpinLoopEnthalpies[30],interiorLoopEnthalpies[30],bulgeLoopEnthalpies[30],tstackEntropies[625],tstackEnthalpies[625],tstack2Entropies[625],tstack2Enthalpies[625];
